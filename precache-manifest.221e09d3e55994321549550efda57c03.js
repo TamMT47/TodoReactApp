bbd7325bcd1de85f2af2bbe1a70f91dd1fe89605
TamMT47 <90486527+TamMT47@users.noreply.github.com>
2022-11-02 23:34:15 +0700

    Updates

---
+++ precache-manifest.221e09d3e55994321549550efda57c03.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c58e32efcd8b37fc7f437965f6f7413d",
+    "url": "/TamMT47/TodoReactApp/index.html"
+  },
+  {
+    "revision": "8cd6c24f4a7f441e618d",
+    "url": "/TamMT47/TodoReactApp/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e360402d4788c8e2ee5c",
+    "url": "/TamMT47/TodoReactApp/static/js/2.339e30f9.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/TamMT47/TodoReactApp/static/js/2.339e30f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8cd6c24f4a7f441e618d",
+    "url": "/TamMT47/TodoReactApp/static/js/main.889e1a28.chunk.js"
+  },
+  {
+    "revision": "7f7aabe0f416cbbe285f",
+    "url": "/TamMT47/TodoReactApp/static/js/runtime-main.8a3cf6fc.js"
+  }
+]);
